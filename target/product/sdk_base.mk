50ef975462fa0b5658ad0c0e8ae571a0ed2ab283
beanstown106 <nbenis106@gmail.com>
2017-01-17 14:34:51 +0800

    stop overwriting audio_effects on pixels

--- target/product/sdk_base.mk
+++ target/product/sdk_base.mk
@@ -73,6 +73,11 @@ PRODUCT_COPY_FILES := \
 	frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
 	hardware/libhardware_legacy/audio/audio_policy.conf:system/etc/audio_policy.conf
 
+ifeq ($(filter marlin sailfish,$(TARGET_PRODUCT)),)
+PRODUCT_COPY_FILES := \
+        frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf
+endif
+
 include $(SRC_TARGET_DIR)/product/emulator.mk
 
 $(call inherit-product-if-exists, frameworks/base/data/sounds/MaterialAudio.mk)
