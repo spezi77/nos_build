f0001532a09e91676eaa616c9eca152825c37731
spezi77 <spezi7713@gmx.net>
2018-07-21 14:27:14 +0200

    Revert "PD'ify the build script."
    This reverts commit 7b7b19e3eb9511d5d55825916f8ce695dc39e19c.

--- core/config.mk
+++ core/config.mk
@@ -960,6 +960,6 @@ endif
 
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/pixeldust/sepolicy/sepolicy.mk)
+$(eval include vendor/nitrogen/sepolicy/sepolicy.mk)
 
 include $(BUILD_SYSTEM)/dumpvar.mk
